7ceca78180fb7bc2263f84b128ed9b2071233e0a
Advita Talekar <advita.18060@sakec.ac.in>
2025-09-12 23:58:15 +0530

    minor changes

--- 800/Make it beautiful.cpp
+++ 800/Make it beautiful.cpp
@@ -2,29 +2,27 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
     int t;
     cin >> t;
     while (t--) {
         int n;
         cin >> n;
         vector<int> a(n);
-        for (int i = 0; i < n; i++) cin >> a[i];
-
-        bool eq = 1;
-        for (int i = 1; i < n; i++) 
-            if (a[i] != a[0]) eq = 0;
-
-        if (eq) {
-            cout << "NO\n";
-            continue;
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
         }
 
-        sort(a.begin(), a.end(), greater<int>());
-        cout << "YES\n";
-        for (int x : a) cout << x << " ";
-        cout << "\n";
+        if (a[0] == a[n - 1]) {
+            cout << "NO\n";
+        } else {
+            cout << "YES\n";
+            cout << a[n - 1] << " ";
+            for (int i = 0; i < n - 1; i++) {
+                cout << a[i] << " ";
+            }
+            cout << "\n";
+        }
     }
+    return 0;
 }
+
