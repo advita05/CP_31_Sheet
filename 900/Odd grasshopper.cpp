af1a414be21f6a153f691f5e9b5c5d146b03d2f9
Advita Talekar <advita.18060@sakec.ac.in>
2025-09-28 00:15:29 +0530

    Odd grasshopper

---
+++ 900/Odd grasshopper.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long x0, n;
+        cin >> x0 >> n;
+        long long d;
+        if (n % 4 == 0) d = 0;
+        else if (n % 4 == 1) d = n;
+        else if (n % 4 == 2) d = -1;
+        else d = -n - 1; 
+
+        long long ans = (x0 % 2 == 0) ? x0 - d : x0 + d;
+        cout << ans << endl;
+    }
+    return 0;
+}
